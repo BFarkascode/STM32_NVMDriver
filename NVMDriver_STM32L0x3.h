232a145b1e32023f749e4ad087dfa4802d1e1608
Balazs Farkas <94067165+BFarkascode@users.noreply.github.com>
2023-10-24 14:35:56 +0200

    Update NVMDriver_STM32L0x3.h

--- NVMDriver_STM32L0x3.h
+++ NVMDriver_STM32L0x3.h
@@ -11,19 +11,23 @@
 #include "stdint.h"
 #include "stm32l053xx.h"
 
+#include "EXTIDriver_STM32L0x3.h"
+
 //LOCAL CONSTANT
 
 //LOCAL VARIABLE
 
 //EXTERNAL VARIABLE
-extern uint32_t Rx_Message_buf [64];
+extern uint32_t Data_buf [16];
 
 //FUNCTION PROTOTYPES
 void NVM_Init (void);
 void FLASHErase_Page(uint32_t flash_page_addr);
 void FLASHUpd_Word(uint32_t flash_word_addr, uint32_t updated_flash_value);
-void FLASHIRQPriorEnable_Custom(void);
+void FLASHIRQPriorEnable(void);
 
-__attribute__((section(".RamFunc"))) void FLASHUpd_HalfPage(uint32_t loc_var_current_flash_half_page_addr, uint8_t full_page_cnt_in_buf, uint8_t half_page_cnt_in_page);		//Note: this function MUST run from RAM, not FLASH!
+__attribute__((section(".RamFunc"))) void FLASHUpd_HalfPage(uint32_t flash_page_addr);
+					//Note: this function MUST run from RAM, not FLASH!
 
 #endif /* INC_NVMDRIVER_STM32L0x3_CUSTOM_H_ */
+
