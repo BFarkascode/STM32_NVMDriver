a88b1393dca33e614d0d93ab96c32eda44c3f05d
Balazs Farkas <94067165+BFarkascode@users.noreply.github.com>
2023-10-24 14:35:03 +0200

    Add files via upload

---
+++ EXTIDriver_STM32L0x3.c
@@ -0,0 +1,135 @@
+/*
+ * EXTIDriver_STM32L0x3.c					v.1.0.
+ *
+ *  Created on: Oct 25, 2023
+ *      Author: Balazs Farkas
+ *
+ *v.1.0
+ *	Below is the definition and the setup of the EXTI channels.
+ *	Callback function names are taken from the device startup file.
+ *	Current version uses PC13 and EXTI13 to engage the blue push button on the L053R8 nucelo board.
+ */
+
+
+#include "EXTIDriver_STM32L0x3.h"
+
+//1)We initialize the EXTIs
+void EXTIInit(void){
+	/*
+	 * Here in this init function we set up the EXTIs following the refman.
+	 * We first activate the input pins, then choose if we wish to have the trigger on the rising or falling edge of the input.
+	 * With the EXTI done, the connected IRQ will always be called when the trigger is activated.
+	 * The trigger's flag must be reset though at the end of the IRQ, otherwise the trigger is not removed and the IRQ remains activated non-stop.
+	 *
+	 * Practical implementation is:
+	 *
+	 * 1)We set the pins up and select the EXTI lines we intend to use
+	 * 2)We enable the syscfg clocking
+	 * 3)We enable the EXTI channels on the pins
+	 * 4)We configure the channels
+	 * 5)We enable the IRQ and set their priority
+	 *
+	 * */
+
+	//1)
+		//Note: below we use PC13 "blue oush button" and thus, EXTI13
+	RCC->IOPENR |= (1<<2);						//PORTC clocking allowed
+	GPIOC->MODER &= ~(1<<26);					//PC13 input - EXTI13
+	GPIOC->MODER &= ~(1<<27);					//PC13 input - EXTI13
+												//we use push-pull
+	GPIOC->OSPEEDR |= (3<<26);					//PC13 very high speed
+												//no pull resistor
+
+	//2)
+	RCC->APB2ENR |= (1<<0);						//SYSCFG enable is on the APB2 enable register
+
+	//3)
+	SYSCFG->EXTICR[4] |= (1<<5);				//since we want to use PC13, we write 0010b to the [7:4] position of the fourth element of the register array - SYSCFG_EXTICR4
+												//Note: EXTICR is uint32_t volatile [4], not just a 32-bit register (similar to AFR)
+	//4)
+	EXTI->IMR |= (1<<13);						//EXTI13 IMR unmasked
+												//we leave the request masked
+	EXTI->RTSR &= ~(1<<5);						//we disable the rising edge
+	EXTI->FTSR |= (1<<5);						//we enable the falling edge
+
+	//5)
+	NVIC_SetPriority(EXTI4_15_IRQn, 1);			//we set the interrupt priority as 1 so it will be lower than the already running DMA IRQ for the SPI
+	NVIC_EnableIRQ(EXTI4_15_IRQn);
+}
+
+
+
+//2)We define the callback function
+void EXTI4_15_IRQHandler (void) {
+	/*
+	 * Note: it is good practice to double check that which pins have activated the EXTI.
+	 *
+	 * 1)Check which pin activated the EXTI
+	 * 2)Act according to the pin activated
+	 * 3)Reset the EXTI
+	 *
+	 * */
+
+	//1)
+	if (EXTI->PR & (1<<13)) {
+
+		//2)
+
+		  //the address we wish to erase
+		  uint32_t flash_page_addr = 0x0800C000;
+
+		  //We check, which value we have been running with (delay values are stored at C014 and C030)
+		  uint32_t* toggle_pointer = 0x0800C014;
+		  toggle_value1 = *toggle_pointer;
+
+		  if (toggle_value1 == 0x00DB23FA) {			//if we were at 2000 ms
+			  toggle_value1 = 0x005B23FA;
+			  toggle_value2 = 0x0018005B;
+		  } else {										//if not
+			  toggle_value1 = 0x00DB23FA;
+			  toggle_value2 = 0x001800DB;
+		  }
+
+		  Data_buf [5] = toggle_value1;					//we update the machine code
+
+		  Data_buf [12] = toggle_value2;
+
+		  //When the trigger happens, we replace the timing of the blink depending on what the original value was.
+		  //Since this modification occurs directly in FLASH, the result will be carried over even after unpowering the system and won't be lost.
+		  //Note: the machine code below may not be right if the original stack is changed and thus the pointers within the machine code would be pointing at the wrong place.
+
+		  //We erase the area where the Blink_custom is
+		  FLASHErase_Page(flash_page_addr);
+
+		  //We rewrite the Blink_custom machine code
+		  //Note: the machine code below may change between code optimisation. One machine code may not work for another code.
+		  //Note: machine code should always be changed bulk!!!
+
+#ifdef word_by_word
+		  //half-page write using individual words
+		  //Note: this version is 16 times slower than using a half-page burst. On the upside, it is very reliable.
+
+		  //The two values below are pointers within the machine code that will also need to be updated if we change between half-page burst and word-by-word writing.
+		  Data_buf [7] = 0x23A0FF07;
+		  Data_buf [13] = 0xFEFAF7F4;
+
+
+		  uint32_t* Data_buf_ptr = Data_buf;
+		  for(int i = 0; i < 16; i++) {					//copying is done word by word. We do need to loop for 16 to reach half a page
+			FLASHUpd_Word(flash_page_addr, *Data_buf_ptr++);
+			flash_page_addr = flash_page_addr + 4;		//we increment the address value by 4. Of not, flash_page_addr is constant, not an array!
+		  }
+#endif
+
+//#ifdef half_page
+		  //half-page full burst
+		  Data_buf [7] = 0x23A0FEB7;
+		  Data_buf [13] = 0xFEAAF7F4;
+		  FLASHUpd_HalfPage(flash_page_addr);
+//#endif
+
+
+		  //3)
+		  EXTI->PR |= (1<<13);						//we reset the IRQ connected to the EXTI13 by writing to the pending bit
+	}
+}
---
+++ main.c
@@ -0,0 +1,307 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stdint.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+#include "NVMDriver_STM32L0x3.h"
+#include "EXTIDriver_STM32L0x3.h"
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+void Blink_PA5_init(void){
+	RCC->IOPENR |=	(1<<0);
+	GPIOA->MODER |= (1<<10);					//output for PA5
+	GPIOA->MODER &= ~(1<<11);					//output for PA5
+}
+
+void Blink_custom(void) {						//this is placed in the APP_MEM memory section defined in the linker file
+												//APP_MEM is 16 kbytes and starts at 0x0800c000
+	GPIOA->BSRR |= (1<<5);
+	HAL_Delay(500);
+//	Delay_ms(200);							//even though we call the Blink with a delay of 2000 ms, we change this value in the FLASH below to 500 ms or 1000 ms
+	GPIOA->BRR |= (1<<5);
+	HAL_Delay(500);
+//	Delay_ms(200);
+}
+
+
+//this is the machine code we will be uploading into the mcu. It is already endian swapped.
+
+uint32_t Data_buf [16] = {
+		  0xAF00B580,
+		  0x05DB23A0,
+		  0x23A0699A,
+		  0x212005DB,
+		  0x619A430A,
+		  0x00DB23FA,
+		  0xF7F40018,
+		  0x23A0FEFD,					//this word often changes when manipulating code - probably a pointer?
+		  0x6A9A05DB,
+		  0x05DB23A0,
+		  0x430A2120,
+		  0x23FA629A,
+		  0x001800DB,
+		  0xFEF0F7F4,					//this word often changes when manipulating code - probably a pointer?
+		  0x46BD46C0,
+		  0x0000BD80
+};
+
+//extern variables defined for the EXTI to use
+uint32_t toggle_value1;
+uint32_t toggle_value2;
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+  Blink_PA5_init();
+  EXTIInit();
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+	  Blink_custom();
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSICalibrationValue = 0;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_5;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2;
+  PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
