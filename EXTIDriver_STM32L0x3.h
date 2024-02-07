76124c0e6deb8ed4ade13ff37eb1766f57563378
Balazs Farkas <94067165+BFarkascode@users.noreply.github.com>
2024-02-07 10:01:39 +0100

    Update EXTIDriver_STM32L0x3.h

--- EXTIDriver_STM32L0x3.h
+++ EXTIDriver_STM32L0x3.h
@@ -1,8 +1,16 @@
 /*
- * EXTIDriver_STM32L0x3.h					v.1.0
  *
  *  Created on: Oct 24, 2023
- *      Author: Balazs Farkas
+ *  Project: STM32_NVMDriver
+ *  File: EXTIDriver_STM32L0x3.h
+ *  Author: BalazsFarkas
+ *  Processor: STM32L053R8
+ *  Compiler: ARM-GCC (STM32 IDE)
+ *  Program version: 1.0
+ *  Program description: N/A
+ *  Hardware description/pin distribution: N/A
+ *  Modified from: N/A
+ *  Change history: N/A
  *
  *      This is a driver for external interrupts.
  *      It is followed by the definition of the callback function of each EXTI.
